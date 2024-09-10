ef7d5f3aadd3f0ae855a16a2ea9d7ea45a85a4f1
Roma Magdy Fawzy <romamagdy3642@gmail.com>
2024-09-10 03:01:51 +0300

    modified app.py port added img to home screen

--- app.py
+++ app.py
@@ -198,4 +198,4 @@ def video():
     return Response(generate_frames(),mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port = 5000)
